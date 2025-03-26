8f0b5b02e54b5bc1116292b4bb1b333bbdca4647
Dayana <dayileyvas@gmail.com>
2025-03-26 09:44:19 -0500

    testing commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function convert() {
+    const inputValue = document.getElementById("userInput").value;
+    const unit = document.getElementById("unit").value;
+    //const milesToKm = true;
+    let result = 0;
+    if (unit === "milesToKm") {
+      result = inputValue * 1.60934;
+    } else {
+      result = inputValue / 1.60934;
+    }
+    const resultString = inputValue + " miles are " + result + " km";
+    console.log(resultString);
+    const resultElement = document.getElementById('resultElement');
+    resultElement.innerHTML = resultString;
+  }
